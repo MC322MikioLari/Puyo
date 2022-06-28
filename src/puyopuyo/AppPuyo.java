f573482d941d505f1228fd30c4400e5c2b4b9314
Gabriel Mikio <gmikioas@gmail.com>
2022-06-28 11:49:01 -0300

    Inserção de um novo puyo puyo na tela de visualização

--- src/puyopuyo/AppPuyo.java
+++ src/puyopuyo/AppPuyo.java
@@ -1,5 +1,11 @@
 package puyopuyo;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+
 public class AppPuyo{
 	final static int MAX_PUYOS = 7*13;
 	final static int widthWindow = 252;
@@ -26,45 +32,76 @@ public class AppPuyo{
 	public static void main(String[] args) {
 	    
 	    while (gameOver == false) {
-		    for (i = 0; i < MAX_PUYOS/2; i++) {
-	    		p[i][0].setY(Celula);
-	    		janela.adicionaImagem(p[i][0]);
-	    		janela.adicionaImagem(p[i][1]);
-	    		try {
-	    			Thread.sleep(1000);
-			    } 
-			    catch (InterruptedException e) {
-			 		System.out.println(e);
+	        try {
+	            AudioInputStream backgroundSound = 
+	            		AudioSystem.getAudioInputStream(AppPuyo.class.getResource("assets/backgroundMusic.wav"));
+	            Clip clipBackground = AudioSystem.getClip();
+	            clipBackground.open(backgroundSound);
+	            clipBackground.start();
+	            clipBackground.loop(Clip.LOOP_CONTINUOUSLY);
+	            // If you want the sound to loop infinitely, then put: clip.loop(Clip.LOOP_CONTINUOUSLY); 
+	            // If you want to stop the sound, then use clip.stop();	    	
+	    	
+			    for (i = 0; i < (MAX_PUYOS/2) - 1; i++) {
+		    		p[i][0].setY(Celula);
+		    		janela.adicionaImagem(p[i][0]); // Primeiro puyo-puyo em seguida
+		    		janela.adicionaImagem(p[i][1]);
+		    		
+		    		p[i+1][0].setY(Celula*2);
+		    		p[i+1][1].setY(Celula*3);
+		    		janela.adicionaImagem(p[i+1][0]);  // Segundo puyo-puyo em seguida
+		    		janela.adicionaImagem(p[i+1][1]);
+		    		try {
+		    			Thread.sleep(1000);
+				    } 
+				    catch (InterruptedException e) {
+				 		System.out.println(e);
+				    }
+			    	for (j = 0; j < 2; j++) {   
+			    		p[i][j].setX(108);
+			    		if (j == 0) {
+			    			p[i][j].setY(-Celula);
+			    		}
+			    		else if (j == 1) {
+			    			p[i][j].setY(-2*Celula);
+			    		}
+			    		p[i][j].setStatus("A");
+			    		p[i][j].setId(i*10+j);
+			    		p[i][j].link(p);
+				   }
+			    	janela.adicionaImagem(p[i][0]);
+			     	janela.adicionaImagem(p[i][1]);
+			     	p[i][0].metro.addActionListener(p[i][0]);
+			     	p[i][1].metro.addActionListener(p[i][1]);
+			     	p[i][0].metro.start();
+			     	p[i][1].metro.start();
+			    	keyboard.link(p[i][0], p[i][1]);
+			    	keyboard.settingEvent();
+			    	metro.start();
+			    	try {
+				 		Thread.sleep(5000);
+				    } 
+				    catch (InterruptedException e) {
+				 		System.out.println(e);
+				    }
+			    	if (p[i][1].getGameOver() == true || p[i][0].getGameOver() == true) {
+			    		clipBackground.stop();
+			    		
+			    		AudioInputStream gameOverAudio = 
+			    				AudioSystem.getAudioInputStream(AppPuyo.class.getResource("assets/gameOver.wav"));
+			            Clip clipGameOver= AudioSystem.getClip();
+			            clipGameOver.open(gameOverAudio);
+			    		JOptionPane.showMessageDialog(janela, "Você Perdeu :(", "Boa sorte na próxima <3", JOptionPane.PLAIN_MESSAGE);
+			    					    		
+			    		System.out.println("GAME OVER");
+			    		gameOver = true;
+			    		janela.GameOver();
+			    	}
 			    }
-		    	for (j = 0; j < 2; j++) {   
-		    		p[i][j].setX(0);
-		    		p[i][0].setY(-Celula);
-		    		p[i][1].setY(-2*Celula);
-		    		p[i][j].setStatus("A");
-		    		p[i][j].setId(i*10+j);
-		    		p[i][j].link(p);
-			   }
-		    	janela.adicionaImagem(p[i][0]);
-		     	janela.adicionaImagem(p[i][1]);
-		     	p[i][0].metro.addActionListener(p[i][0]);
-		     	p[i][1].metro.addActionListener(p[i][1]);
-		     	p[i][0].metro.start();
-		     	p[i][1].metro.start();
-		    	keyboard.link(p[i][0], p[i][1]);
-		    	keyboard.settingEvent();
-		    	metro.start();
-		    	try {
-			 		Thread.sleep(5000);
-			    } 
-			    catch (InterruptedException e) {
-			 		System.out.println(e);
-			    }
-		    	if (p[i][1].getGameOver() == true || p[i][0].getGameOver() == true) {
-		    		System.out.println("GAME OVER");
-		    		gameOver = true;
-		    		janela.GameOver();
-		    	}
-		    }
-	    }
+		    }catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	    }	    
+	    
     }
 }
