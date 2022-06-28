dd255ea0f2caacf3b609a21a3baf73a30d0f7378
Gabriel Mikio <gmikioas@gmail.com>
2022-06-28 14:20:06 -0300

    Revert "Merge branch 'main' of https://github.com/MC322MikioLari/Puyo"
    This reverts commit ca6184c99cc7bcd96a8f100e17d572120c217db7, reversing
    changes made to 7cdc5e315262894370f8aff082a07884d8caa420.

--- src/puyopuyo/Board.java
+++ src/puyopuyo/Board.java
@@ -1,6 +1,11 @@
 package puyopuyo;
 
-import javax.swing.JOptionPane;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 import javax.swing.JPanel;
 
@@ -10,11 +15,11 @@ public class Board extends JPanel{
 	final static int widthTotal = 400;
 	final static int widthWindow = 252;
 	final static int heightWindow = 468;
-	final static int WIDTH = widthWindow/7;
-	final static int HEIGHT = widthWindow/7;
-	final static int Celula = widthWindow/7;
+	//private static final long serialVersionUID = 1278136335268310294L;
+	String score;
+	int s = 0;
+	int layout = 0; //Layout 0 � quando !GameOver
 	
-	// tecnica para identificar o diretorio em que a classe esta
 	public static String DIRETORIO =
 	         AppPuyo.class.getResource(".").getPath();
 	public void paintComponent(Graphics g2){
@@ -37,72 +42,19 @@ public class Board extends JPanel{
 		}
 	}
 	
-	public static Maker maker = new Maker();
-	public static int i, j;
-	public static Puyo[][] p = maker.makePuyos();
-	public static Notifier metro = Singleton.getInstance();
-	public static Keyboard keyboard = new Keyboard(metro);
-	public static Placar placar = new Placar();
-	public static JanelaImagem janela = new JanelaImagem(keyboard, placar);
-	public static Sound sound= new Sound();
-    public static Remove removePuyos = new Remove(p, metro, placar, janela, sound);
-    public static DescePuyo descePuyo = new DescePuyo(p, metro);
-    public static boolean gameOver = false;
-    
-	public static void main(String[] args) {
-	    while (gameOver == false) {
-	    	sound.BackgroundSound();
-			for (i = 0; i < (MAX_PUYOS/2) - 1; i++) {
-				p[i][0].setY(Celula);
-				p[i][1].setY(0);
-				janela.adicionaImagem(p[i][0]); // Primeiro puyo-puyo em seguida
-				janela.adicionaImagem(p[i][1]);
-				
-				p[i+1][0].setY(Celula*2);
-				p[i+1][1].setY(Celula*3);
-				janela.adicionaImagem(p[i+1][0]);  // Segundo puyo-puyo em seguida
-				janela.adicionaImagem(p[i+1][1]);
-				try {
-					Thread.sleep(1000);
-				} 
-				catch (InterruptedException e) {
-					System.out.println(e);
-				}
-				for (j = 0; j < 2; j++) {   
-					p[i][j].setX(108);
-					if (j == 0) {
-						p[i][j].setY(-Celula);
-					}
-					else if (j == 1) {
-						p[i][j].setY(-2*Celula);
-					}
-					p[i][j].setStatus("A");
-					p[i][j].setId(i*10+j);
-					p[i][j].link(p);
-				}
-		    	janela.adicionaImagem(p[i][0]);
-		     	janela.adicionaImagem(p[i][1]);
-		     	p[i][0].metro.addActionListener(p[i][0]);
-		     	p[i][1].metro.addActionListener(p[i][1]);
-		     	p[i][0].metro.start();
-		     	p[i][1].metro.start();
-		    	keyboard.link(p[i][0], p[i][1]);
-		    	keyboard.settingEvent();
-		    	metro.start();
-		    	try {
-			 		Thread.sleep(5000);
-			    } 
-			    catch (InterruptedException e) {
-			 		System.out.println(e);
-			    }
-		    	if (p[i][1].getGameOver() == true || p[i][0].getGameOver() == true) {
-		    		sound.GameOverSound();
-		    		JOptionPane.showMessageDialog(janela, "Voce Perdeu :(", "Boa sorte na proxima <3", JOptionPane.PLAIN_MESSAGE);
-		    		System.out.println("GAME OVER");
-		    		gameOver = true;
-		    		janela.GameOver();
-		    	}
-		    }
-	    }
-    }
-}
+	public void ChangePlacar(int score) {
+		s += score;
+		this.validate();
+	}
+	public void changeLayout(int i) {
+		layout = i;
+	}
+	public void GameOver(Graphics g2) {
+		super.paintComponent(g2);
+		g2.setColor(Color.black);
+		g2.fillRect(0, 0, widthTotal, heightWindow);
+		g2.setColor(Color.white);
+		g2.setFont(new Font("serif", Font.BOLD, 20));
+		g2.drawString("GAME OVER", 125, heightWindow/2);
+	}
+ }
